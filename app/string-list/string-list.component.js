5c66575518a4712334e462a9e25890721f507c5f
iqarchli <ismail.qarchli@gmail.com>
2020-04-12 22:03:10 +0200

    strings html skeleton

--- app/string-list/string-list.component.js
+++ app/string-list/string-list.component.js
@@ -9,10 +9,29 @@
         controller: ['$http', '$scope',
           function stringListController($http,$scope) {
              
-              var vm = this; 
-
-             
-             
+            var vm = this;
+            vm.disChar = true;
+            
+            
+            vm.options = [
+              {name:'Lower Case', id:1},
+              {name:'Upper Case', id:2},
+              {name:'Lower Case [a-z] & numbers', id:3},
+              {name:'All letters [a-zA-Z] & numbers', id:4},
+              {name:'All letters [a-z.A-Z]', id:5},
+              {name:'Digits [0-9]', id:5},
+              {name:'Custom', id:6}
+            ];
+            vm.strOpt = 6;
+              
+           vm.setTest = function setTest() {
+            if(vm.strOpt.id == 6)
+                vm.disChar = false;
+            else
+                vm.disChar = true;
+           };
+              
           }
+        
         ]
       });
